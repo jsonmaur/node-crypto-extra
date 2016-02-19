ddbceeee05b594fcefc8affdb7480e32bdcec1ca
Jason Maurer <j@maur.co>
2016-02-19 14:11:21 -0700

    open sourced

---
+++ src/index.js
@@ -0,0 +1,97 @@
+/**
+ * dependencies
+ */
+
+import crypto, { randomBytes } from 'crypto'
+import bcryptjs from 'bcryptjs'
+import dn from 'denodeify'
+
+export default crypto
+
+/**
+ * encrypt a value using AES256-CTR
+ * @param value (string) the string to encrypt
+ * @param key (string, optional) the private key to use
+ */
+
+export function encrypt (value, key) {
+  /* only allow strings and numbers */
+  if (typeof value === 'number') value = String(value)
+  else if (typeof value === 'object') value = JSON.stringify(value)
+  else if (typeof value !== 'string') return
+
+  key = key || process.env.ENCRYPTION_KEY
+
+  let cipher = crypto.createCipher('aes-256-ctr', key)
+  let encrypted = cipher.update(value, 'utf8', 'hex')
+  encrypted += cipher.final('hex')
+
+  return encrypted
+}
+
+/**
+ * decrypt a value using AES256-CTR
+ * @param value (string) the encrypted string to decrypt
+ * @param key (string, optional) the private key to use
+ */
+
+export function decrypt (value, key) {
+  if (typeof value !== 'string') return
+
+  key = key || process.env.ENCRYPTION_KEY
+
+  let decipher = crypto.createDecipher('aes-256-ctr', key)
+  let decrypted = decipher.update(value, 'hex', 'utf8')
+  decrypted += decipher.final('utf8')
+
+  if (decrypted.indexOf('{') === 0) {
+    decrypted = JSON.parse(decrypted)
+  }
+
+  return decrypted
+}
+
+/**
+ * create a hash
+ * @param value (string) the value to hash
+ * @param algorithm (string, optional) the hashing algorithm to use
+ */
+
+export function hash (value, algorithm = 'sha256') {
+  return crypto.createHash(algorithm)
+    .update(value)
+    .update('salt')
+    .digest('hex')
+}
+
+/**
+ * for hashing to bcrypt, and comparing to a hash.
+ * @param value (string) the value to hash
+ * @param hash (string, optional) if provided, the hash to compare
+ */
+
+export function bcrypt (value, hash) {
+  if (hash) {
+    return dn(bcryptjs.compare)(value, hash)
+  } else {
+    return dn(bcryptjs.genSalt)(10)
+      .then(salt => dn(bcryptjs.hash)(value, salt))
+  }
+}
+
+/**
+ * creates a random hex value
+ * @param size (int, optional) the size of the returned string
+ */
+
+export function random (size = 32) {
+  if (size === 0) {
+    throw new Error('zero-length randomHex is useless')
+  }
+
+  if (size % 2 !== 0) {
+    throw new Error('randomHex size must be even')
+  }
+
+  return randomBytes(size / 2).toString('hex')
+}
---
+++ tests/index.test.js
@@ -0,0 +1,90 @@
+import test from 'blue-tape'
+import * as crypto from '../src/index'
+
+test('crypto', t => {
+
+  t.test('encrypt()', st => {
+    st.plan(4)
+    st.equal(
+      crypto.encrypt('hey', 'secret'),
+      '32950d',
+      'encrypt a string')
+    st.equal(
+      crypto.encrypt(100, 'secret'),
+      '6bc044',
+      'encrypt a number')
+    st.equal(
+      crypto.encrypt({hello: 'hey'}, 'secret'),
+      '21d21c968c2fa0fe37128f41803069',
+      'encrypt an object')
+    st.equal(
+      crypto.encrypt(true, 'secret'),
+      undefined,
+      'encrypt an invalid value')
+  })
+
+  t.test('decrypt()', st => {
+    st.plan(2)
+    st.equal(
+      crypto.decrypt('32950d', 'secret'),
+      'hey',
+      'decrypt a string')
+    st.deepEqual(
+      crypto.decrypt('21d21c968c2fa0fe37128f41803069', 'secret'),
+      {hello: 'hey'},
+      'decrypt an object')
+  })
+
+  t.test('hash()', st => {
+    st.plan(2)
+    st.equal(
+      crypto.hash('hey'),
+      'baeb867c70224060d6f46ed1a8d7f5eab765c1785048afd7c37e320e7f86eeda',
+      'hash a string with sha256')
+    st.equal(
+      crypto.hash('hey', 'md5'),
+      '8d4e533358362e22c742567ec589143a',
+      'hash a string with md5')
+  })
+
+  t.test('bcrypt()', st => {
+    return crypto.bcrypt('hey')
+      .then(hash => {
+        st.equal(
+          typeof hash,
+          'string',
+          'should be a string')
+        st.not(
+          hash.indexOf('$'),
+          -1,
+          'should include a dollar sign')
+      })
+  })
+
+  t.test('bcrypt() compare', st => {
+    let hashed = '$2a$10$Y9k5MdpqPHx5ZCV7F7sq/OytrUY.uwUJURLgTiTdknJso.in/Qnr2'
+    return crypto.bcrypt('hey', hashed)
+      .then(res => st.ok(res, 'hashes match'))
+      .then(() => crypto.bcrypt('heyy', hashed))
+      .then(res => st.notOk(res, 'hashes do not match'))
+  })
+
+  t.test('random()', st => {
+    st.plan(4)
+    st.equal(
+      crypto.random().length,
+      32,
+      'random with length of 32')
+    st.equal(
+      crypto.random(64).length,
+      64,
+      'random with length of 64')
+    st.throws(
+      () => crypto.random(0),
+      'random with length 0')
+    st.throws(
+      () => crypto.random(31),
+      'random with odd length')
+  })
+
+})
